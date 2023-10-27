f5379abd22dc8a956938a3e2a968f5fef8ae26f3
Anderson Alzate <aaalzate5@misena.edu.co>
2023-10-27 16:09:06 -0500

    readme

---
+++ Menu/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> New Order</title>
+	
+	<link rel="stylesheet" href="../Menu/style.css">
+	<link rel="shortcut icon" href="../images/logoact.jpg" type="image/x-icon">
+	
+	<form action="../index.html" method="post">  
+
+<p>
+  <input type="image" src="../images/logoact.jpg" width="150px" top="6.5px">       
+</p>
+</form>
+
+			
+<h1 style="text-align:center;">Bienvenido a New Order</h1>
+
+
+	<a href="staff" >Empleado</a>
+	<a href="admin">Administrador</a>
+  
+
+
+
+
+
+</head>
+<body>
+</div>
+<!-- partial -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js'></script>
+
+
+	
+</body>
+</html>
